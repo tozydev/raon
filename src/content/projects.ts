880d3f076359555aaca9076ece2cac31350dc204
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-07-13 18:14:16 +0700

    refactor: add getProjects featured filter option

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -1,9 +1,24 @@
 import { getCollection, type CollectionEntry } from "astro:content"
 
+export type Project = CollectionEntry<"projects">
+
+/**
+ * Options for fetching projects.
+ */
+interface GetProjectsOptions {
+  /**
+   * If true, only fetch projects that are marked as featured.
+   */
+  featured?: boolean
+}
+
 /**
  * Fetches all projects from the "projects" collection and sorts them in ascending order based on their ID.
- * @returns {Promise<CollectionEntry<"projects">[]>} A promise that resolves to an array of sorted project entries.
+ * @param {GetProjectsOptions} [options] - Optional parameters to filter the projects.
+ * @returns {Promise<Project[]>} A promise that resolves to an array of sorted project entries.
  */
-export async function getProjects(): Promise<CollectionEntry<"projects">[]> {
-  return (await getCollection("projects")).toSorted((a, b) => a.id.localeCompare(b.id))
+export async function getProjects(options?: GetProjectsOptions): Promise<Project[]> {
+  return (
+    await getCollection("projects", (project) => !options?.featured || project.data.featured)
+  ).toSorted((a, b) => a.id.localeCompare(b.id))
 }
